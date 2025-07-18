a8f2e10dbe8c2bc085ab9a9d6245c6ba94f7b163
gkirber <george.kirber@gmail.com>
2025-07-18 23:13:24 +0200

    Getting latitude and longitude data

--- init.js
+++ init.js
@@ -1,5 +1,7 @@
 import { switchTheme } from './components/switchTheme.js'
+import { getGeoData } from './api/geoData.js'
 
 export function initApp() {
 	switchTheme()
+	getGeoData()
 }
--- src/api/apiKeyAndHost.js
+++ src/api/apiKeyAndHost.js
@@ -1,3 +1,3 @@
 export const apiKey = 'd079ed9263b8db4e2961b1d68a8ab85a'
 
-export const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
+export const baseUrl = 'https://api.openweathermap.org'
